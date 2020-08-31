5f81c41cb9753912da29a028a8165e63d5ab42b4
rosie-murillo <rosie.murillo96@gmail.com>
2020-08-30 23:34:12 -0700

    Adding camera intent and permissions to manifest

--- app/src/main/java/com/example/picturecreator/MainActivity.kt
+++ app/src/main/java/com/example/picturecreator/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.picturecreator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val REQUEST_IMAGE_CAPTURE = 1
+    }
 
     lateinit var folderName: EditText
     lateinit var cameraPicture: Button
@@ -17,7 +23,16 @@ class MainActivity : AppCompatActivity() {
         folderName = findViewById(R.id.editTextTextPersonName)
 
         cameraPicture.setOnClickListener {
-
+            dispatchTakePictureIntent()
         }
     }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
 }
